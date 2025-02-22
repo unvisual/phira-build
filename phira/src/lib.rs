b57613a940723de9248c85b825fc6d4e4eb79c5d
unvisual <helloworld2b@outlook.com>
2025-02-22 15:34:08 +0800

    LibRS

--- phira/src/lib.rs
+++ phira/src/lib.rs
@@ -21,6 +21,20 @@ mod tags;
 mod threed;
 mod uml;
 
+#[cfg(target_os = "android")]
+#[no_mangle]
+pub unsafe extern "C" fn Java_quad_1native_QuadNative_preprocessInput(
+    _: *mut std::ffi::c_void,
+    _: *const std::ffi::c_void,
+    #[allow(dead_code)] motionEvent: ndk_sys::AInputEvent,
+    #[allow(dead_code)] f: ndk_sys::jfloat,
+    #[allow(dead_code)] f2: ndk_sys::jfloat,
+    #[allow(dead_code)] z: ndk_sys::jboolean,
+    #[allow(dead_code)] z2: ndk_sys::jboolean,
+) {
+    
+}
+
 use anyhow::Result;
 use data::Data;
 use macroquad::prelude::*;
